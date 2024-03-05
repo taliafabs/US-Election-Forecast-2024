66b896a877676d9f09b903a3ffbe1cb356d575b4
talia <talia.fabregas@mail.utoronto.ca>
2024-03-05 22:07:54 +0000

    raw data

---
+++ 00-simulation-survey.R
@@ -0,0 +1,30 @@
+#### Preamble ####
+# Purpose: Simulate the Survey Data Set
+# Author: Talia Fabregas
+# Date: 5 March 2024
+# Contact: talia.fabregas@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# Any other information needed? 
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(janitor)
+library(dplyr)
+
+presidential_choices_2020 <- c("Donald Trump", 
+                               "Joe Biden", 
+                               "Howie Hawkins", 
+                               "Did not vote")
+party_affiliation <- c("Democrat", 
+                       "Republican")
+gender <- c("Male", "Female")
+marital_status <- c("single", "married", "divorced", "widowed")
+
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+
+
