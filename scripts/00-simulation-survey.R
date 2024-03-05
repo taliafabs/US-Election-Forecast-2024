399d6b8cbe39628d0d7d7ba4ccba896f94094ef9
talia <talia.fabregas@mail.utoronto.ca>
2024-03-05 22:19:20 +0000

    add simulation and data cleaning R scripts

--- scripts/00-simulation-survey.R
+++ scripts/00-simulation-survey.R
@@ -1,17 +1,18 @@
 #### Preamble ####
 # Purpose: Simulate the Survey Data Set
-# Author: Talia Fabregas
+# Author: Talia Fabregas, Fatimah Yunusa, Aamishi
 # Date: 5 March 2024
 # Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
-# Any other information needed? 
+# Any other information needed?
 
 
 #### Workspace setup ####
 library(tidyverse)
 library(janitor)
 library(dplyr)
+
 
 presidential_choices_2020 <- c("Donald Trump", 
                                "Joe Biden", 
---
+++ scripts/01-data_cleaning-survey.R
@@ -0,0 +1,10 @@
+#### Preamble ####
+# Purpose: Prepare and clean the survey data downloaded from ...
+# Author: Talia Fabregas, Fatimah Yunusa, Aamishi
+# Date: 5 March 2024
+# Contact: talia.fabregas@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Download and save the survey data from ---
+# - Add the file to the gitignore
+# Any other information needed?
