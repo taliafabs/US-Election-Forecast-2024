77ea3a8c57811aca44e7447531552cedffc18bec
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-03-06 19:25:50 -0500

    added code to survey data cleaning

--- scripts/01-data_cleaning-survey.R
+++ scripts/01-data_cleaning-survey.R
@@ -8,3 +8,14 @@
 # - Download and save the survey data from ---
 # - Add the file to the gitignore
 # Any other information needed?
+
+#### Workplace Setup ####
+library(janitor)
+library(tidyverse)
+# Read in the raw data
+raw_survey_data <- read_csv("data/raw_data/2024_week3_Jan-12--Jan-19.csv")
+# Add labels
+raw_survey_data <- labelled::to_factor(raw_survey_data)
+
+# Choose the variables that we want
+reduced_survey_data <- raw_survey_data 
