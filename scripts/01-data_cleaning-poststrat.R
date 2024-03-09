379e9b9b99553701da22542f494c318a92333048
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-03-09 16:23:20 -0500

    data cleaninng so far

--- scripts/01-data_cleaning-poststrat.R
+++ scripts/01-data_cleaning-poststrat.R
@@ -6,7 +6,39 @@
 # Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
+# - Download the ACS 2022 data and save it to data/raw_data
+# - Gitignore it
 # Any other information needed?
 
 
+#### Workplace Setup ####
+library(tidyverse)
+library(janitor)
+library(haven)
+
+# Read in the raw post stratification data
+raw_poststrat_data <- read_dta("data/raw_data/usa_00001.dta")
+
+# Add labels
+raw_poststrat_data <- labelled::to_factor(raw_poststrat_data)
+
+# Select relevant variables
+reduced_poststrat_data1 <- raw_poststrat_data |>
+  select(
+    birthyr,
+    age,
+    sex,
+    race,
+    hispan,
+    racwht,
+    racasian,
+    racblk,
+    racamind,
+    educ,
+    marst,
+    labforce,
+    ftotinc,
+    stateicp,
+    metro
+  )
 
--- scripts/01-data_cleaning-survey.R
+++ scripts/01-data_cleaning-survey.R
@@ -5,43 +5,87 @@
 # Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
-# - Download and save the survey data from ---
+# - Download the America's Pulse Survey Data Week 3 Jan 12-19
+# 2024 from Polarization Research Lab and save it to data/raw_data
 # - Add the file to the gitignore
 # Any other information needed?
 
 #### Workplace Setup ####
-library(janitor)
 library(tidyverse)
+library(janitor)
+library(haven)
+
 # Read in the raw data
 raw_survey_data <- read_csv("data/raw_data/2024_week3_Jan-12--Jan-19.csv")
 # Add labels
 raw_survey_data <- labelled::to_factor(raw_survey_data)
-raw_survey_data$
-# Choose the variables that we want
-# pid7, gender, birthyr, race, hispanic, speakspanish, educ, marstat, employ, faminc_new,
-# pid3, ideo5, pres16vote, pres20vote, inputstate
-reduced_survey_data <- raw_survey_data |>
-  select(
-    pid7,
-    gender,
-    birthyr,
-    race,
-    hispanic,
-    faminc_new,
-    educ,
-    inputstate
-  )
+
+# add an age column
+raw_survey_data <- raw_survey_data |>
+  mutate(age = 2024 - birthyr)
+
+# Select relevant columns
+reduced_survey_data1 <- raw_survey_data |>
+  select(pid3,
+         pid7,
+         presvote16post,
+         presvote20post,
+         ideo5,
+         birthyr,
+         age,
+         gender,
+         race,
+         hispanic,
+         educ,
+         marstat,
+         employ, 
+         faminc_new,
+         inputstate,
+         urbanicity2
+         )
 
 # clean state names to match IPUMS ACS post stratification data set
+names_matcher <- tibble(stateicp = state.name, inputstate = state.abb)
 
-# filter out undecided or voting for someone other than trump or biden
+# Filter out people who:
+#   - 1. have no party affiliation or leaning  
+#   - 2. did not vote for Trump or Biden in 2020
+# it is not practical to decipher their 2024 vote choice.
 
-# vote_biden = 1 if Biden, 0 if Trump
-# create the following age brackets:
-# reformat gender variable
-# group education levels
-# group races
-# make hispanic a binary variable
+reduced_survey_data2 <- reduced_survey_data1 |>
+  filter(presvote20post == "Joe Biden" | 
+           presvote20post == "Donald Trump" |
+           pid7 != "Independent")
 
-# change column formats
+reduced_survey_data2 <- reduced_survey_data2 |>
+  mutate(
+    age_groups = case_when(age < 25 ~ "18-24",
+                           age < 35 ~ "25-34",
+                           age < 50 ~ "36-49",
+                           age < 65 ~ "50-64",
+                           age < 80 ~ "65-79",
+                           age >= 80 ~ "80+"),
+    vote_biden = ifelse((presvote20post == "Joe Biden" | 
+                           pid7 == "Not very strong Democrat" |
+                           pid7 == "Lean Democrat" |
+                           pid7 == "Strong Democrat"), 
+                        1, 
+                        0),
+    sex = ifelse(gender == "Female", 1, 0),
+    # indicator variables for race
+    race_white = ifelse(race == "White", 1, 0),
+    race_asian = ifelse(race == "Asian", 1, 0),
+    race_black = ifelse(race == "Black", 1, 0),
+    race_hispanic = ifelse(race == "Hispanic", 1, 0),
+    race_native = ifelse(race == "Native American", 1, 0),
+    race_mideast = ifelse(race == "Middle Eastern", 1, 0),
+    urban = ifelse((urbanicity2 == "Big city"| urbanicity == "Smaller city"), 1, 0)
+  )
 
+# do something with the education level here
+# reduced_survey_data2 |> 
+#   mutate(
+#     education_level = casewhen(
+#       
+#     )
+#   )
