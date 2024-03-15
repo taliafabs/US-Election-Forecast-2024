33835813346ec490029a1c516cb3156b8eb48a3d
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-03-15 18:37:30 -0400

    model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -21,36 +21,10 @@ library(modelsummary)
 #### Read Data ####
 survey_analysis_data <- read_parquet("data/analysis_data/survey_analysis_data.parquet")
 
-# set seed for repoducibility
+# set seed for reproducibility
 set.seed(853)
 
 ### Model Data ###
-# model1 <- 
-#   glm(
-#     formula = vote_biden ~ age + sex + races + education_level + faminc_new + inputstate + urban,
-#     data = survey_analysis_data,
-#     family = binomial(link = "logit")
-#     # prior = normal(location = 0, scale = 2.5, autoscale=TRUE),
-#     # prior_intercept = normal(location=0, scale=2.5, autoscale=TRUE),
-#     # seed = 853
-#   )
-# 
-# model2 <- 
-#   glm(
-#     formula = vote_biden ~ age + sex + races,
-#     data = survey_analysis_data,
-#     family = binomial(link="logit")
-#     # family = binomial(link = "logit"),
-#     # prior = normal(location = 0, scale = 2.5, autoscale=TRUE),
-#     # prior_intercept = normal(location=0, scale=2.5, autoscale=TRUE),
-#     # seed = 853
-#   )
-# 
-# model3 <- glm(
-#   formula = vote_biden ~ age + sex + race_asian + race_black + race_hispanic + inputstate,
-#   data = survey_analysis_data,
-#   family = binomial(link = "logit") 
-# )
 
 # Want to explain vote choice based on various explanatory variables
 # Use logistic regression
@@ -58,35 +32,15 @@ set.seed(853)
 
 # start with simpler model then make it more complicated if needed
 # state seems to be causing the warnings but this is not something we want to exclude
+# removed state for now
 presidential_vote_model <- stan_glm(
-  formula = vote_biden ~ sex + age_bracket + races + education_level + state + urban,
+  formula = vote_biden ~ sex + age_bracket + races + education_level + urban,
   data = survey_analysis_data,
   family = binomial(link="logit"),
   prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
   prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
   seed = 853
 )
-
-# # alternative option
-# # simpler model
-# # does not take urban or rural into account
-# presidential_vote_model1 <- stan_glm(
-#   formula = vote_biden ~ sex + age_bracket + races + education_level + urban,
-#   data = survey_analysis_data,
-#   family = binomial(link="logit"),
-#   prior = normal(location=0, scale=2.5, autoscale=TRUE),
-#   prior_intercept = normal(location=0, scale=2.5, autoscale=TRUE),
-#   seed = 853
-# )
-# 
-# presidential_vote_model_state <- stan_glm(
-#   formula = vote_biden ~ sex + age_bracket + state,
-#   data = survey_analysis_data,
-#   family = binomial(link="logit"),
-#   prior = normal(location=0, scale=2.5, autoscale=TRUE),
-#   prior_intercept = normal(location=0, scale=2.5, autoscale=TRUE),
-#   seed = 853
-# )
 
 # coefficients <- broom::tidy(presidential_vote_model, conf.int = T)
 
